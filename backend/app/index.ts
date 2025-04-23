22b74d90c4880b617a2412b01968d33c7fc9a5a5
Esh <32418603+Esh07@users.noreply.github.com>
2025-04-23 19:25:51 +0100

    chore: Add initial index.ts file with express and body-parser setup

---
+++ backend/app/index.ts
@@ -0,0 +1,15 @@
+// import express from 'express'
+// import { PrismaClient } from '@prisma/client'
+
+// const prisma = new PrismaClient()
+const compression = require('compression')
+import express from 'express'
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(compression())
+
+// A route declaration section
+
+export { app };
