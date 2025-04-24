bd6b7a8e913691b9745868a2aa0e66aed6ca803a
Esh <32418603+Esh07@users.noreply.github.com>
2025-04-24 23:27:38 +0100

    feat: Add CORS middleware to enable cross-origin requests

--- backend/app/index.ts
+++ backend/app/index.ts
@@ -1,18 +1,31 @@
+// import express from 'express'
+// import { PrismaClient } from '@prisma/client'
+
+// const prisma = new PrismaClient()
 const compression = require('compression')
 import express from 'express'
 const bodyParser = require('body-parser')
 const router = express.Router();
+const cookieParser = require("cookie-parser");
 
-
-
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(compression());
+app.use(cookieParser());
+app.use(express.json());
+
+// Use CORS middleware
+app.use(cors({
+    origin: 'http://localhost:5173', // Allow requests from this origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+}));
 
 // A route declaration section
 const { auth } = require('../api/auth/auth.routes');
-console.log(auth)
+// console.log(auth)
 app.use('/auth', auth);
 
 const { user } = require('../api/users/users.routes');
@@ -20,10 +33,13 @@ const { user } = require('../api/users/users.routes');
 app.use('/user', user);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-    console.log('Hello World!')
+    res.status(200).json({ message: 'Hello World!' });
+
+    // console.log('Hello World!')
 }
 );
+
+
 
 
 export { app };
