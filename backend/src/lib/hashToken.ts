86b94bff7f62a01b62b29f0abc99778fa2538c6d
Esh <32418603+Esh07@users.noreply.github.com>
2025-04-24 01:05:56 +0100

    fix: Update hashToken function export to named export and remove default export

--- backend/src/lib/hashToken.ts
+++ backend/src/lib/hashToken.ts
@@ -1,8 +1,6 @@
 const crypto = require('crypto');
 
 
-function hashToken(token: string) {
+export function hashToken(token: string) {
   return crypto.createHash('sha512').update(token).digest('hex');
 }
-
-export default { hashToken };
