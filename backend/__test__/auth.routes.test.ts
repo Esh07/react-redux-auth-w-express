504eca64cc90603a67c74031af1e9e1ed4c175f3
Esh <32418603+Esh07@users.noreply.github.com>
2025-04-26 09:07:09 +0100

    refactor(tests): update endpoints and fix test cases - Refactored test cases to use new endpoints:   - /user/register instead of /auth/register   - /user/login instead of /auth/login   - /user/logout instead of /auth/logout
    - Fixed test cases to account for additional fields in responses:
      - /user/login now returns accessToken
      - /user endpoint now includes createdAt and IsAdmin fields
    
    - Updated assertions to match the new response structures

--- backend/__test__/auth.routes.test.ts
+++ backend/__test__/auth.routes.test.ts
@@ -70,8 +70,8 @@ describe("Auth Register User routes", () => {
  * Test "/auth/Register" POST route
  * Expected to fail because the route is not defined
  * */
-  it("should fail GET /auth/register", async () => {
-    const response = await request(app).get("/auth/register");
+  it("should fail GET /user/register", async () => {
+    const response = await request(app).get("/user/register");
     expect(response.status).toBe(404);
   });
 
@@ -79,8 +79,8 @@ describe("Auth Register User routes", () => {
     * Test "/auth/register" POST route
     * Expected to pass because the route is defined
     * */
-  it("should successfully POST /auth/register", async () => {
-    const response = await request(app).post("/auth/register").send(userDataFields);
+  it("should successfully POST /user/register", async () => {
+    const response = await request(app).post("/user/register").send(userDataFields);
     expect(response.status).toBe(201);
     // it only receives access token and refresh token
     expect(response.body).toEqual({ accessToken: expect.any(String), refreshToken: expect.any(String) });
@@ -90,8 +90,8 @@ describe("Auth Register User routes", () => {
     * Test with same email "/auth/register" POST route
     * Expected to fail because the same email is already in the database
     * */
-  it("should fail POST /auth/register with same email", async () => {
-    const response = await request(app).post("/auth/register").send(userDataFields);
+  it("should fail POST /user/register with same email", async () => {
+    const response = await request(app).post("/user/register").send(userDataFields);
     expect(response.status).toBe(400);
     expect(response.body).toEqual({ message: "User with this email already exists." });
   });
@@ -100,38 +100,41 @@ describe("Auth Register User routes", () => {
 // Test User routes
 describe("Login User routes", () => {
 
-  it("should fail POST /auth/login with wrong password", async () => {
+  it("should fail POST /user/login with wrong password", async () => {
     const email = userDataFields.email;
-    const response = await request(app).post("/auth/login").send({ email, password: "wrongpassword" });
+    const response = await request(app).post("/user/login").send({ email, password: "wrongpassword" });
     expect(response.status).toBe(400);
 
     expect(response.body).toEqual({ message: "Invalid email or password." });
   }
   );
 
-  it("should successfully POST /auth/login", async () => {
+  it("should successfully POST /user/login", async () => {
 
-    const response = await request(app).post("/auth/login").send(loginData);
+    const response = await request(app).post("/user/login").send(loginData);
 
     // fetch the token from httpOnly cookie using cookie-parser
     const cookies = cookie.parse(response.headers["set-cookie"][0]);
     token = cookies.token;
 
     expect(response.status).toBe(200);
-    expect(response.body).toEqual({ message: "Logged in successfully." });
+    expect(response.body).toEqual({ message: "Logged in successfully.", accessToken: expect.any(String) });
+    // test the accessToken  in body 
+
+
   });
 
   //try to register after logging in to see if it fails
-  it("should fail POST /auth/register after logging in", async () => {
+  it("should fail POST /user/register after logging in", async () => {
 
     // login to get the token 
-    const loginResponse = await request(app).post("/auth/login").send(loginData);
+    const loginResponse = await request(app).post("/user/login").send(loginData);
     const cookies = cookie.parse(loginResponse.headers["set-cookie"][0]);
     token = cookies.token;
 
     // Attempt to register with the new user data
     const response = await request(app)
-      .post("/auth/register")
+      .post("/user/register")
       .set("Cookie", `token=${token}`)
       .send({
         name: "John Doe",
@@ -149,20 +152,20 @@ describe("Login User routes", () => {
 
 describe("Logout_routes", () => {
 
-  it("should successfully POST /auth/login", async () => {
+  it("should successfully POST /user/login", async () => {
 
-    const response = await request(app).post("/auth/login").send(loginData);
+    const response = await request(app).post("/user/login").send(loginData);
 
     // fetch the token from httpOnly cookie using cookie-parser
     const cookies = cookie.parse(response.headers["set-cookie"][0]);
     token = cookies.token;
 
     expect(response.status).toBe(200);
-    expect(response.body).toEqual({ message: "Logged in successfully." });
+    expect(response.body).toEqual({ message: "Logged in successfully.", accessToken: expect.any(String) });
   });
 
-  it("should successfully POST /auth/logout", async () => {
-    const response = await request(app).post("/auth/logout").set("Cookie", `token=${token}`);
+  it("should successfully POST /user/logout", async () => {
+    const response = await request(app).post("/user/logout").set("Cookie", `token=${token}`);
     expect(response.status).toBe(200);
     expect(response.body).toEqual({ message: "Logged out successfully." });
   });
@@ -176,15 +179,39 @@ describe("Other routes", () => {
   //   // Test "/user" GET route
   it("GET /profile should return logged in user", async () => {
     // login to get the token
-    const loginResponse = await request(app).post("/auth/login").send(loginData);
+    const loginResponse = await request(app).post("/user/login").send(loginData);
     const cookies = cookie.parse(loginResponse.headers["set-cookie"][0]);
     token = cookies.token;
 
-    const response = await request(app).get("/user/profile").set("Cookie", `token=${token}`);
+    const response = await request(app).get("/user").set("Cookie", `token=${token}`);
     expect(response.status).toBe(200);
     expect(response.body).toEqual(expect.objectContaining({
-      email: loginData.email,
-      name: userDataFields.name,
+      user: expect.objectContaining({
+        IsAdmin: false,
+        createdAt: expect.any(String),
+        email: loginData.email,
+        name: userDataFields.name,
+      }),
     }));
   });
+
+  //   // Test "/user/:id" GET route
+  //   it("should return a single user", async () => {
+  //     const response = await request(app).get(`/user/${userDataFields.email}`);
+  //     expect(response.status).toBe(200);
+  //     expect(response.body).toEqual(userDataFields);
+  //   });
+
+  //   // Test "/user" POST route
+  //   it("should create a new user", async () => {
+  //     const newUser = {
+  //       name: "John Doe",
+  //       email: "test1@exmaple.com",
+  //         password: "password",
+  //     };
+  //     const response = await request(app).post("/user").send(newUser);
+  //     expect(response.status).toBe(201);
+  //     expect(response.body).toEqual({ ...newUser, id: expect.any(Number) });
+  //     }
+  //     );
 });
